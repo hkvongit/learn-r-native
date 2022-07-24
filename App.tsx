bd704c52ed4e7bd75af724d1305d98a911bd641e
hari-anitha <brosgiant@gmail.com>
2022-07-24 22:46:07 +0530

    Todo app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import Todo from "./screens/Todo";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,8 +18,11 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        {/* <Navigation colorScheme={colorScheme} /> */}
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <Todo />
+        </ApplicationProvider>
+        {/* <StatusBar /> */}
       </SafeAreaProvider>
     );
   }
